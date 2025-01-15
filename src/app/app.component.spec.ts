e9900b61a1f9cfade3d1f65f4350d98cd678e151
Marcos Piemontez <mpiemontez@eleventickets.com>
2025-01-15 19:52:55 -0400

    rename project to 'login-page' and update related files; add global styles

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'initial_angular_studies' title`, () => {
+  it(`should have the 'login-page' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('initial_angular_studies');
+    expect(app.title).toEqual('login-page');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, initial_angular_studies');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, login-page');
   });
 });
